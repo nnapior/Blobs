fd5e0ea79ed5307fe4b191370b2a3841d2553215
Nick Napior <nnapior@gmail.com>
2020-10-13 18:34:53 -0400

    added instructions to index

---
+++ public/js/leaderboard.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAvdLzE32-S6BKfG1PpAUgVVdprHIiIxNQ",
+  authDomain: "blobs-1fd04.firebaseapp.com",
+  projectId: "blobs-1fd04",
+  databaseURL: "https://blobs-1fd04.firebaseio.com/",
+  storageBucket: "blobs-1fd04.appspot.com"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
